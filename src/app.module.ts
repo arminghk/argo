4e7f2fa42511ef66f7e6bb174cec7f4c2db8d937
arminghk <armin@aspireteq.se>
2025-02-24 15:47:42 +0330

    init passenger rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CustomConfigModule } from './modules/config/config.module';
+import { PassengerModule } from './modules/passenger/passenger.module';
+
 
 
 @Module({
-  imports: [],
+  imports: [CustomConfigModule, PassengerModule],
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
